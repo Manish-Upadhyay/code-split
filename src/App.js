3c1913278b4572071421aa4bd414bdb87c00e1c4
Manish-Upadhyay <manish.upadhyay2017@outlook.com>
2019-11-14 18:26:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Loading from "./Loading";
+import DynamicImport from "./DynamicImport";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Home = props => (
+  <DynamicImport load={() => import("./component/Home")}>
+    {Component => (Component === null ? <Loading /> : <Component {...props} />)}
+  </DynamicImport>
+);
+
+const Topics = props => (
+  <DynamicImport load={() => import("./component/Topics")}>
+    {Component => (Component === null ? <Loading /> : <Component {...props} />)}
+  </DynamicImport>
+);
+
+const Settings = props => (
+  <DynamicImport load={() => import("./component/Settings")}>
+    {Component => (Component === null ? <Loading /> : <Component {...props} />)}
+  </DynamicImport>
+);
+
+const LazyHome = React.lazy(() => import("./component/Home"));
+const LazyTopics = React.lazy(() => import("./component/Topics"));
+const LazySettings = React.lazy(() => import("./component/Settings"));
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/topics">Topics</Link>
+            </li>
+            <li>
+              <Link to="/settings">Settings</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          <Route exact path="/" component={Home} />
+          <Route path="/topics" component={Topics} />
+          <Route path="/settings" component={Settings} />
+
+          <React.Suspense fallback={<Loading />}>
+            <Route exact path="/" component={LazyHome} />
+            <Route path="/topics" component={LazyTopics} />
+            <Route path="/settings" component={LazySettings} />
+          </React.Suspense>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/Loading.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { Skeleton } from "antd";
+
+export class Loading extends Component {
+  render() {
+    return <Skeleton />;
+  }
+}
+
+export default Loading;
